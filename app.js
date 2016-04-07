3aef2d6afbd7e3b819e6c411a86897b0353c29d7
Jarhf <3203591@163.com>
2016-04-07 15:52:53 +0800

    made some changes

--- app.js
+++ app.js
@@ -17,8 +17,24 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.use(function(req, res, next){
-    
+//cors策略中间件
+app.use(function (req, res, next) {
+    var origin = req.header("Origin");
+    var method = req.method;
+    if (origin) {
+        res.header("Access-Control-Allow-Origin", origin);//不过滤是否是受信源了，dcloud做的客户端不知origin是不是固定的
+        res.header("Access-Control-Allow-Method", method);
+
+        var corsHeader = req.header("Access-Control-Request-Headers");
+        if (corsHeader) {
+            res.setHeader("Access-Control-Allow-Headers", corsHeader);
+        }
+        res.header("Access-Control-Allow-Credentials", "true");
+    }
+    if (method.toUpperCase() == "OPTIONS") {//OPTIONS请求，不应该执行后续操作，所以直接结束响应
+        res.end();
+        return;//直接返回，不用next下一个中间件了
+    }
     next();
 });
 
@@ -32,10 +48,10 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -43,23 +59,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,60 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.all('/news/findNews.json', function(req, res, next) {
+    //res.render('index', { title: 'Express' });
+    var data = {
+        "newsList": [
+            {
+                "id": 3,
+                "title": "测试5",
+                "sourceUrl": "news.html?id=3",
+                "source": "测试5",
+                "createTime": 1458089053000,
+                "newsType": 1,
+                "fdUserId": null
+            },
+            {
+                "id": 2,
+                "title": "测试4",
+                "sourceUrl": "news.html?id=2",
+                "source": "测试4",
+                "createTime": 1458002630000,
+                "newsType": 1,
+                "fdUserId": null
+            },
+            {
+                "id": 1,
+                "title": "测试3",
+                "sourceUrl": "news.html?id=1",
+                "source": "测试3",
+                "createTime": 1458002604000,
+                "newsType": 1,
+                "fdUserId": null
+            }
+        ]
+    };
+   
+    res.json(data);
+});
+
+router.put('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.delete('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.patch('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.all('/', function(req, res, next) {
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
